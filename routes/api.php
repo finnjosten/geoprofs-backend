b87be23ab9c8ef81f533032360a0bbdfd2ece93e
BlackSparowYT <1203142@student.roc-nijmegen.nl>
2024-09-26 10:56:13 +0200

    Update api.php
    Changed it so when the app is in local mode

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,14 @@ Route::get('/user', function (Request $request) {
 Route::get('/', function () { abort(404); });
 
 Route::group(['prefix' => 'auth'], function () {
-    Route::post('/login',           [AuthController::class, 'login']    )->name('login');
-    Route::post('/logout',          [AuthController::class, 'logout']   )->name('logout')->middleware('auth:sanctum');;
+    Route::post('/login',   [AuthController::class, 'login']    )->name('login');
+    Route::post('/logout',  [AuthController::class, 'logout']   )->name('logout')->middleware('auth:sanctum');
 });
 
-Route::get('users/',                [UserController::class, 'index']    )->name('users.index')->middleware('auth:sanctum');;
-Route::post('users/create',         [UserController::class, 'store']    )->name('users.create');
-Route::get('users/{id}',            [UserController::class, 'show']     )->name('users.show')->middleware('auth:sanctum');;
-Route::post('users/{id}/update',    [UserController::class, 'update']   )->name('users.update')->middleware('auth:sanctum');;
-Route::delete('users/{id}/delete',  [UserController::class, 'destroy']  )->name('users.delete')->middleware('auth:sanctum');;
+// Allow creating users without auth when app is in local mode
+if (env('APP_ENV') == 'local') Route::post('users/create',[UserController::class, 'store'])->name('users.create');
+if (env('APP_ENV') != 'local') Route::post('users/create',[UserController::class, 'store'])->name('users.create')->middleware('auth:sanctum');
+Route::get      ('users/',                  [UserController::class, 'index']    )->name('users.index')->middleware('auth:sanctum');
+Route::get      ('users/{id}',              [UserController::class, 'show']     )->name('users.show')->middleware('auth:sanctum');
+Route::post     ('users/{id}/update',       [UserController::class, 'update']   )->name('users.update')->middleware('auth:sanctum');
+Route::delete   ('users/{id}/delete',       [UserController::class, 'destroy']  )->name('users.delete')->middleware('auth:sanctum');
