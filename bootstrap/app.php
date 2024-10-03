b2b235dbddb35b1dc63b8f7818ea382f644c9cc3
Jordi Verbeek <jordi.verbeek@gmail.com>
2024-10-03 12:17:13 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->pushMiddleware(Cors::class);
+        $middleware->append(Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
