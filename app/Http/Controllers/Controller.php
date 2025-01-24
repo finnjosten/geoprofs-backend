3d76d825b451cd96c2252995a66e0d8bd170e204
Finn Josten <finn.r.josten@proton.me>
2025-01-24 11:47:29 +0100

    Check role function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Request;
+
 abstract class Controller {
-    //
+
+    public function checkPermission(Array $roles, $return_type = 'response') {
+        $user_role = Request::user()->role_slug;
+
+        if (!in_array($user_role, $roles)) {
+
+            if ($return_type == 'response') {
+                return response()->json([
+                    "error" => "Unauthorized",
+                    "code" => "unauthorized",
+                    "message" => "You are not authorized to perform this action"
+                ], 401);
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
 }
